cb36e6da756dbf1ee2bf6913d42b8334baa0d872
Fitrahtur Rahman <fitrahtur@gmail.com>
2024-12-18 19:44:02 +0700

    update deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lpindonesia.github.io',
-  base: '/',
   integrations: [tailwind()],
 });

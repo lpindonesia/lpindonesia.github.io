fecb9ee36089886f546f5fbb67ba5e1be10c55ff
Fitrahtur Rahman <fitrahtur@gmail.com>
2025-05-23 20:17:46 +0700

    create past events data

---
+++ src/data/past_events.ts
@@ -0,0 +1,47 @@
+import reconnect2020 from '../assets/events/reconnect-2020.jpg'
+import rememberingChester2017 from '../assets/events/20170730-remembering-chester.jpg'
+import charity2012 from '../assets/events/20130106-charity.jpg'
+import tribute2013 from '../assets/events/20131026-tribute.jpg'
+import tribute2025 from '../assets/events/20250111-tribute.jpg'
+import gathering20250126 from '../assets/events/20250126-gathering.jpg'
+
+const EVENTS = [
+  {
+    title: 'LP Fans Semarang Gathering',
+    date: '26 January 2025',
+    img: gathering20250126,
+    alt: 'LP Fans Semarang Gathering',
+  },
+  {
+    title: 'Millennium Tribute Night Party Jakarta',
+    date: '11 January 2025',
+    img: tribute2025,
+    alt: 'Millennium Tribute Night Party Jakarta',
+  },
+  {
+    title: 'LP Indonesia Fans Virtual Gathering',
+    date: '10 October 2020',
+    img: reconnect2020,
+    alt: 'LP Indonesia Fans Virtual Gathering',
+  },
+  {
+    title: '#LPMemorialJakarta - Remembering Chester',
+    date: '30 July 2017',
+    img: rememberingChester2017,
+    alt: '#LPMemorialJakarta - Remembering Chester',
+  },
+  {
+    title: 'A Tribute to Linkin Park 2013',
+    date: '26 October 2013',
+    img: tribute2013,
+    alt: 'A Tribute to Linkin Park 2013',
+  },
+  {
+    title: 'Bogor Charity 2013',
+    date: '6 January 2013',
+    img: charity2012,
+    alt: 'Bogor Charity 2013',
+  },
+]
+
+export default EVENTS
